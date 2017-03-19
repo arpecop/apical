64da744bd05be25dd4249aa94744b94c2ee58c11
Radoslav <rudix.lab@gmail.com>
2017-03-20 00:28:01 +0200

    c

--- mashable.js
+++ mashable.js
@@ -340,14 +340,21 @@ function newsapi(dummy, callback) {
                             console.log('📦 delivered ' + src);
                             newsapix('entertainment-weekly', 'latest', function (src) {
                                 console.log('📦 delivered ' + src);
-
                                 newsapix('hacker-news', 'latest', function (src) {
                                     console.log('📦 delivered ' + src);
-                                    newsapix('reddit-r-all', 'latest', function (src) {
+                                    newsapix('google-news', 'top', function (src) {
                                         console.log('📦 delivered ' + src);
-                                        newsapix('independent', 'top', function (src) {
+                                        newsapix('ign', 'latest', function (src) {
                                             console.log('📦 delivered ' + src);
-                                            callback();
+                                            newsapix('independent', 'top', function (src) {
+                                                console.log('📦 delivered ' + src);
+                                                newsapix('mirror', 'latest', function (src) {
+                                                    newsapix('the-lad-bible', 'latest', function (src) {
+                                                        console.log('📦 delivered ' + src);
+                                                        callback();
+                                                    });
+                                                });
+                                            });
                                         });
                                     });
                                 });
