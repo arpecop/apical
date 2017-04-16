d2f701aa8579a8f61b2d70ca42d76dfd3b127342
Radoslav <rudix.lab@gmail.com>
2017-04-16 18:54:02 +0300

    c

--- _includes/pouch.js
+++ _includes/pouch.js
@@ -1,8 +1,8 @@
 const PouchDB = require('pouchdb-node');
 
-const mystbox = new PouchDB('./db/mystic');
-const bgusers = new PouchDB('./db/bgusers');
-const poparticles = new PouchDB('./db/poparticles');
+const mystbox = new PouchDB('/app/_includes/db/mystic');
+const bgusers = new PouchDB('/app/_includes/db/bgusers');
+const poparticles = new PouchDB('/app/_includes/db/poparticles');
 module.exports = {
     mystbox: mystbox,
     bgusers: bgusers,
