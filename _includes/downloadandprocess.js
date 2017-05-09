e889c0da0bef32779656eada41fa03a9ffb057df
Radoslav <rudix.lab@gmail.com>
2017-05-09 23:05:40 +0300

    c

--- _includes/downloadandprocess.js
+++ _includes/downloadandprocess.js
@@ -31,7 +31,7 @@ function post_img(url, callback) {
       if (!err) {
         let jsxon = JSON.parse(body);
 
-        console.log(jsxon.data.image.original.url)
+        console.log(jsxon)
         callback({
           url: jsxon.data.image.original.url.replace('originals', '236x'),
           url_big: jsxon.data.image.original.url,
