be2b62b0832b1604ac4617820a050f76246d8c57
Radoslav <rudix.lab@gmail.com>
2017-06-16 02:33:52 +0300

    c

--- _includes/pouch.js
+++ _includes/pouch.js
@@ -4,9 +4,7 @@ const allusers = new PouchDB(__dirname + '/db/allusers');
 
 function gimmethousend(db, callback) {
   allusers.get("0_" + db, function(err, count) {
-    console.log("0_" + db);
 
-    console.log(err || count);
 
     let rd = Math.floor(Math.random() * count.total) + 0;
     allusers.get(db + '_' + rd, function(err, chunk) {
