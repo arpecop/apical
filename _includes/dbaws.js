00848886b78fed93167a1f4c8a518f99ad9655fd
Rudi <rudix@mac.local>
2017-08-02 13:34:03 +0300

    c

--- _includes/dbaws.js
+++ _includes/dbaws.js
@@ -8,67 +8,76 @@ const db = new PouchDB('http://1:1@robco.herokuapp.com/db');
 const db1 = new PouchDB('http://1:1@robco.herokuapp.com/content');
 
 function put(jsonx, callback) {
-  db.get(jsonx._id, function (err, old_doc) {
-    var json = {
-      _id: jsonx._id ? jsonx._id : new Date().getTime() + '_' + Math.floor((Math.random() * 10) + 1),
-      type: jsonx.type ? (jsonx.type || jsonx._id) : undefined,
-      time: new Date().getTime(),
-      _rev: err ? undefined : old_doc._rev,
-      value: jsonx
-    };
-    db.put(json, function (err, cap) {
-      callback(null, cap);
+  db
+    .get(jsonx._id, function (err, old_doc) {
+      var json = {
+        _id: jsonx._id
+          ? jsonx._id
+          : new Date().getTime() + '_' + Math.floor((Math.random() * 10) + 1),
+        type: jsonx.type
+          ? (jsonx.type || jsonx._id)
+          : undefined,
+        time: new Date().getTime(),
+        _rev: err
+          ? undefined
+          : old_doc._rev,
+        value: jsonx
+      };
+      db.put(json, function (err, cap) {
+        callback(null, cap);
+      });
     });
-  });
 }
 
 function get(id, callback) {
   if (id) {
-    let sid = (typeof id === 'object') ? id.id || id._id : id;
+    let sid = (typeof id === 'object')
+      ? id.id || id._id
+      : id;
     if (id.limit) {
-      db.query('i/' + id.id, Object.assign(id, {
-        'descending': true,
-        'skip': id.skip ? id.skip : 0,
-        'start_key': id.gt ? id.gt : undefined
-      }), function (err, body) {
-        if (!err) {
-          var arr = [];
-          Promise.all(body.rows.map(function (item) {
-            return new Promise(function (cb, rj) {
-              arr.push(Object.assign(item.value.value, {
-                key: item.id,
-                id: item.id,
-                _date: new Date(item.value.time)
-              }));
-              cb()
-            });
-          })).then(function (data) {
-            callback(null, {
-              docs: arr
-            });
-          });
-        } else {
-          callback({})
-        }
-      });
+      db
+        .query('i/' + id.id, Object.assign(id, {
+          'descending': true,
+          'skip': id.skip
+            ? id.skip
+            : 0,
+          'start_key': id.gt
+            ? id.gt
+            : undefined
+        }), function (err, body) {
+          if (!err) {
+            var arr = [];
+            Promise.all(body.rows.map(function (item) {
+              return new Promise(function (cb, rj) {
+                arr.push(Object.assign(item.value.value, {
+                  key: item.id,
+                  id: item.id,
+                  _date: new Date(item.value.time)
+                }));
+                cb()
+              });
+            }))
+              .then(function (data) {
+                callback(null, {docs: arr});
+              });
+          } else {
+            callback(err)
+          }
+        });
     } else {
-      db.get(sid, function (err, doc) {
-        if (err) {
-          callback({}, {})
-        } else {
-          callback(null, Object.assign(doc.value, {
-            _id: doc._id
-          }))
-        }
-      })
+      db
+        .get(sid, function (err, doc) {
+          if (err) {
+            callback({}, {})
+          } else {
+            callback(null, Object.assign(doc.value, {_id: doc._id}))
+          }
+        })
     }
   } else {
     callback({}, {})
   }
 }
-
-
-
 
 //dsd
 
---
+++ _statii.js
@@ -0,0 +1,150 @@
+const request = require("request");
+const fs = require("fs");
+const get = require("get");
+const async = require("async");
+const shortid = require("shortid");
+const _ = require("underscore");
+const extend = require("extend");
+const sizeOf = require("image-size");
+
+const db = require(__dirname + "/_includes/dbaws.js");
+const pagestoget = require(__dirname + "/_includes/source_statii.json");
+const pages = require(__dirname + "/_includes/pages.json");
+
+const downloadnprocess = require(__dirname + "/_includes/downloadandprocess.js");
+const promo = require(__dirname + "/_includes/promo.js");
+
+function datex(prefix) {
+  var coeff = 1000 * 60 * 3;
+  var date = new Date(); //or use any other date
+  var rounded = new Date(Math.round(date.getTime() / coeff) * coeff);
+  var d = date.getDate();
+  var m = date.getMonth();
+  var h = date.getHours();
+  var m1 = date.getMinutes();
+  var y = date.getFullYear();
+  return prefix + "" + y + "" + m + "" + d + "" + h + "" + m1;
+}
+//
+Array.prototype.chunk = function (n) {
+  if (!this.length) {
+    return [];
+  }
+  return [this.slice(0, n)].concat(this.slice(n).chunk(n));
+};
+
+function post(id, callback) {
+  count = 0;
+  counterr = 0;
+
+  db.get({
+    id: 'newsbg',
+    limit: 3,
+    gt: id
+  }, function (err, posts) {
+    console.log(posts.docs[0]);
+
+    db.get(id, function (err, doc) {
+      async
+        .eachSeries(_.shuffle(pages), function (page, callbackx) {
+          request
+            .post("https://graph.facebook.com/" + page.id + "/feed", {
+              form: {
+                published: process.env.PORT
+                  ? 1
+                  : 0,
+                link: "http://apps.facebook.com/infobord/" + id,
+                name: posts.docs[0].name,
+                description: posts.docs[0].description,
+                title: posts.docs[0].title,
+                link: "http://apps.facebook.com/infobord/" + posts.docs[0]._id,
+                picture: posts.docs[0].url_big,
+                access_token: page.access_token
+              }
+            }, function (error, response, body) {
+
+              let resp = JSON.parse(body);
+              if (resp.error) {
+                counterr++;
+              } else {
+                count++;
+              }
+              callbackx();
+            });
+        }, function done() {
+          console.log("📘 posted to facebook pages news 🚨:" + counterr + " ✅:" + count);
+          callback();
+        });
+
+    });
+  })
+}
+
+function statii(lat, callback) {
+
+  async
+    .each(pagestoget.rows, function (item, callbackx) {
+      var rtoken = pages[Math.floor(Math.random() * pages.length + 0)].access_token;
+      //var rtoken = process.env.izvestie_token;
+      var url = "https://graph.facebook.com/" + item.id + "/feed?access_token=" + rtoken + "&fields=id,likes,type,full_picture&limit=1";
+      request(url, function (error, response, body) {
+
+        var collect = [];
+        if (!error && response.statusCode == 200) {
+          async
+            .each(JSON.parse(body).data, function (item, callback1) {
+
+              if (item.likes && item.type === "link") {
+                request("https://graph.facebook.com/" + item.id + "?access_token=" + rtoken + "&fields=full_picture,message,link,name,created_time", function (error, response, body) {
+                  var insertjson = JSON.parse(body);
+                  insertjson.type = 'newsbg';
+                  insertjson.title = insertjson.name;
+                  insertjson.description = insertjson.message;
+                  insertjson.url = insertjson.picture;
+                  insertjson.source = insertjson.link;
+                  insertjson.url_big = insertjson.full_picture;
+                  insertjson._id = new Date(insertjson.created_time).getTime() + '_1';
+
+                  db.get(insertjson._id, function (err, data) {
+                    if (err) {
+                      db
+                        .put(insertjson, function (err, zer) {
+
+                          post(insertjson._id, function (zzmata) {
+
+                            callback1();
+                          });
+
+                        });
+                    } else {
+                      callback1();
+                    }
+                  });
+                })
+              } else {
+                callback1();
+              }
+            }, function done() {
+              callbackx();
+            });
+        } else {
+          callbackx();
+        }
+      });
+    }, function done() {
+      callback();
+    });
+}
+
+if (!process.env.PORT) {
+  statii('1', function (data) {
+    console.log(data);
+
+  })
+
+}
+
+module.exports = {
+  statii: statii
+};
+//
