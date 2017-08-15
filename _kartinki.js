2323121ec2ca6ae0d5c03cfc096acdbc71279318
Rudi <rudix@mac.local>
2017-08-15 15:37:19 +0300

    c

--- _kartinki.js
+++ _kartinki.js
@@ -1,31 +1,31 @@
-const request = require("request");
-const fs = require("fs");
-const get = require("get");
-const async = require("async");
-const shortid = require("shortid");
-const _ = require("underscore");
-const extend = require("extend");
-const sizeOf = require("image-size");
+const request = require('request');
+const fs = require('fs');
+const get = require('get');
+const async = require('async');
+const shortid = require('shortid');
+const _ = require('underscore');
+const extend = require('extend');
+const sizeOf = require('image-size');
 
-const db = require(__dirname + "/_includes/dbaws.js");
-const pagestoget = require(__dirname + "/_includes/source.json");
-const pages = require(__dirname + "/_includes/pages.json");
+const db = require(`${__dirname}/_includes/dbaws.js`);
+const pagestoget = require(`${__dirname}/_includes/source.json`);
+const pages = require(`${__dirname}/_includes/pages.json`);
 
-const downloadnprocess = require(__dirname + "/_includes/downloadandprocess.js");
-const promo = require(__dirname + "/_includes/promo.js");
+const downloadnprocess = require(`${__dirname}/_includes/downloadandprocess.js`);
+const promo = require(`${__dirname}/_includes/promo.js`);
 
-var template = "тази снимка от приятел става популярна.";
+const template = 'тази снимка от приятел става популярна.';
 
 function datex(prefix) {
-  var coeff = 1000 * 60 * 3;
-  var date = new Date(); //or use any other date
-  var rounded = new Date(Math.round(date.getTime() / coeff) * coeff);
-  var d = date.getDate();
-  var m = date.getMonth();
-  var h = date.getHours();
-  var m1 = date.getMinutes();
-  var y = date.getFullYear();
-  return prefix + "" + y + "" + m + "" + d + "" + h + "" + m1;
+  const coeff = 1000 * 60 * 3;
+  const date = new Date(); // or use any other date
+  const rounded = new Date(Math.round(date.getTime() / coeff) * coeff);
+  const d = date.getDate();
+  const m = date.getMonth();
+  const h = date.getHours();
+  const m1 = date.getMinutes();
+  const y = date.getFullYear();
+  return `${prefix}${y}${m}${d}${h}${m1}`;
 }
 //
 Array.prototype.chunk = function (n) {
@@ -40,38 +40,36 @@ function post(id, callback) {
   counterr = 0;
 
   db.get({
-    id: "bgimgsx",
-    limit: 4
-  }, function (e, doc_old) {
-    console.log('https://s-media-cache-ak0.pinimg.com/236x/' + doc_old.docs[2].img);
+    id: 'bgimgsx',
+    limit: 4,
+  }, (e, doc_old) => {
+    console.log(`https://s-media-cache-ak0.pinimg.com/236x/${doc_old.docs[2].img}`);
 
-    db.get(id, function (err, doc) {
-      //      promo.post(id, process.env.izvestie_token, template, "bgusers", function
-      // () {
+    db.get(id, (err, doc) => {
       async
-        .each(_.shuffle(pages), function (page, callbackx) {
+        .each(_.shuffle(pages), (page, callbackx) => {
           request
-            .post("https://graph.facebook.com/" + page.id + "/feed", {
+            .post(`https://graph.facebook.com/${page.id}/feed`, {
               form: {
                 published: process.env.PORT
                   ? 1
                   : 0,
-                link: "http://pix.fbook.space/" + id,
+                link: `http://pix.fbook.space/${id}`,
                 child_attachments: [
                   {
-                    link: "https://pix.fbook.space/" + id,
-                    picture: 'https://s-media-cache-ak0.pinimg.com/400x/' + doc.img
+                    link: `https://pix.fbook.space/${id}`,
+                    picture: `https://s-media-cache-ak0.pinimg.com/400x/${doc.img}`,
                   }, {
-                    link: "https://pix.fbook.space/" + doc_old.docs[2]._id,
-                    picture: 'https://s-media-cache-ak0.pinimg.com/400x/' + doc_old.docs[2].img
-                  }
+                    link: `https://pix.fbook.space/${doc_old.docs[2]._id}`,
+                    picture: `https://s-media-cache-ak0.pinimg.com/400x/${doc_old.docs[2].img}`,
+                  },
                 ],
-                access_token: page.access_token
-              }
-            }, function (error, response, body) {
+                access_token: page.access_token,
+              },
+            }, (error, response, body) => {
               console.log(body);
 
-              let resp = JSON.parse(body);
+              const resp = JSON.parse(body);
               if (resp.error) {
                 counterr++;
               } else {
@@ -79,73 +77,69 @@ function post(id, callback) {
               }
               callbackx();
             });
-        }, function done() {
-          console.log("📘 posted to facebook pages 🚨:" + counterr + " ✅:" + count);
+        }, () => {
+          console.log(`📘 posted to facebook pages 🚨:${counterr} ✅:${count}`);
           callback();
         });
-      // });
+    // });
     });
   });
 }
 
+
 function kartinki(lat, callback) {
-
-  async
-    .each(pagestoget.rows, function (item, callbackx) {
-      var rtoken = pages[Math.floor(Math.random() * pages.length + 0)].access_token;
-      //var rtoken = process.env.izvestie_token;
-      var url = "https://graph.facebook.com/v2.6/" + item.id + "/feed?access_token=" + rtoken + "&fields=id,likes,type,full_picture&limit=1";
-      request(url, function (error, response, body) {
-
-        var collect = [];
-        if (!error && response.statusCode == 200) {
-          async
-            .each(JSON.parse(body).data, function (item, callback1) {
-              if (item.likes && item.likes.data.length >= 20 && item.type === "photo") {
-                db
-                  .db1
-                  .get(item.id, function (err, data) {
-                    if (err) {
-                      db
-                        .db1
-                        .put({
-                          _id: item.id
-                        }, function (err, zer) {
-                          downloadnprocess
-                            .go(item.full_picture, "bgimgsx", function (shortie) {
-                              post(shortie, function (zzmata) {
-                                callback1();
-                              });
+  async.each(pagestoget.rows, (item, callbackx) => {
+    const rtoken = pages[Math.floor(Math.random() * pages.length + 0)].access_token;
+    // var rtoken = process.env.izvestie_token;
+    const url = `https://graph.facebook.com/v2.6/${item.id}/feed?access_token=${rtoken}&fields=id,likes,type,full_picture&limit=1`;
+    request(url, (error, response, body) => {
+      const collect = [];
+      if (!error && response.statusCode == 200) {
+        async
+          .each(JSON.parse(body).data, (item, callback1) => {
+            if (item.likes && item.likes.data.length >= 20 && item.type === 'photo') {
+              db
+                .db1
+                .get(item.id, (err, data) => {
+                  if (err) {
+                    db
+                      .db1
+                      .put({
+                        _id: item.id,
+                      }, (err, zer) => {
+                        downloadnprocess
+                          .go(item.full_picture, 'bgimgsx', (shortie) => {
+                            post(shortie, (zzmata) => {
+                              callback1();
                             });
-                        });
-                    } else {
-                      callback1();
-                    }
-                  });
-              } else {
-                callback1();
-              }
-            }, function done() {
-              callbackx();
-            });
-        } else {
-          callbackx();
-        }
-      });
-    }, function done() {
-      callback();
+                          });
+                      });
+                  } else {
+                    callback1();
+                  }
+                });
+            } else {
+              callback1();
+            }
+          }, () => {
+            callbackx();
+          });
+      } else {
+        callbackx();
+      }
     });
+  }, () => {
+    callback();
+  });
 }
 
 if (!process.env.PORT) {
-  kartinki('1', function (data) {
-    console.log(data);
-
-  })
-
+  kartinki('1', (data) => {
+    console.log(data); // dsad
+  });
 }
 
 module.exports = {
-  kartinki: kartinki
+  kartinki,
 };
 //
--- _statii.js
+++ _statii.js
@@ -1,38 +1,25 @@
-const request = require("request");
-const fs = require("fs");
-const get = require("get");
-const async = require("async");
-const shortid = require("shortid");
-const _ = require("underscore");
-const extend = require("extend");
-const sizeOf = require("image-size");
+const request = require('request');
+const fs = require('fs');
+const get = require('get');
+const async = require('async');
+const shortid = require('shortid');
+const _ = require('underscore');
+const extend = require('extend');
+const sizeOf = require('image-size');
 
-const db = require(__dirname + "/_includes/dbaws.js");
-const pagestoget = require(__dirname + "/_includes/source_statii.json");
-const pages = require(__dirname + "/_includes/pages.json");
+const db = require(`${__dirname}/_includes/dbaws.js`);
+const pagestoget = require(`${__dirname}/_includes/source_statii.json`);
+const pages = require(`${__dirname}/_includes/pages.json`);
+const promo = require(`${__dirname}/_includes/promo.js`);
 
-// const downloadnprocess = require(__dirname +
-// "/_includes/downloadandprocess.js");
-const promo = require(__dirname + "/_includes/promo.js");
 
-function datex(prefix) {
-  var coeff = 1000 * 60 * 3;
-  var date = new Date(); //or use any other date
-  var rounded = new Date(Math.round(date.getTime() / coeff) * coeff);
-  var d = date.getDate();
-  var m = date.getMonth();
-  var h = date.getHours();
-  var m1 = date.getMinutes();
-  var y = date.getFullYear();
-  return prefix + "" + y + "" + m + "" + d + "" + h + "" + m1;
-}
-//
 Array.prototype.chunk = function (n) {
   if (!this.length) {
     return [];
   }
   return [this.slice(0, n)].concat(this.slice(n).chunk(n));
 };
+
 
 function post(id, callback) {
   count = 0;
@@ -41,158 +28,141 @@ function post(id, callback) {
   db.get({
     id: 'newsbg',
     limit: 10,
-    gt: id
-  }, function (err, posts) {
-
-    db
-      .get(id, function (err, doc) {
-        async
-          .each(_.shuffle(pages), function (page, callbackx) {
-
-            request
-              .post("https://graph.facebook.com/" + page.id + "/feed", {
-                form: {
-                  published: process.env.PORT
-                    ? 1
-                    : 0,
-                  link: "https://newsboy.fbook.space/",
-                  child_attachments: [
-                    {
-                      'description': posts.docs[0].description,
-                      'name': posts.docs[0].name,
-                      'link': "https://newsboy.fbook.space/" + id,
-                      'picture': posts.docs[0].url_big
-                    }, {
-                      'description': posts.docs[1].description,
-                      'name': posts.docs[1].name,
-                      'link': "https://newsboy.fbook.space/" + posts.docs[1].id,
-                      'picture': posts.docs[1].url_big
-                    }, {
-                      'description': posts.docs[2].description,
-                      'name': posts.docs[2].name,
-                      'link': "https://newsboy.fbook.space/" + posts.docs[2].id,
-                      'picture': posts.docs[2].url_big
-                    }, {
-                      'description': posts.docs[3].description,
-                      'name': posts.docs[3].name,
-                      'link': "https://newsboy.fbook.space/" + posts.docs[1].id,
-                      'picture': posts.docs[3].url_big
-                    }, {
-                      'description': posts.docs[4].description,
-                      'name': posts.docs[4].name,
-                      'link': "https://newsboy.fbook.space/" + posts.docs[4].id,
-                      'picture': posts.docs[4].url_big
-                    }, {
-                      'description': posts.docs[5].description,
-                      'name': posts.docs[5].name,
-                      'link': "https://newsboy.fbook.space/" + posts.docs[5].id,
-                      'picture': posts.docs[5].url_big
-                    }, {
-                      'description': posts.docs[6].description,
-                      'name': posts.docs[6].name,
-                      'link': "https://newsboy.fbook.space/" + posts.docs[6].id,
-                      'picture': posts.docs[6].url_big
-                    }, {
-                      'description': posts.docs[7].description,
-                      'name': posts.docs[7].name,
-                      'link': "https://newsboy.fbook.space/" + posts.docs[7].id,
-                      'picture': posts.docs[7].url_big
-                    }
-                  ],
-                  access_token: page.access_token
-                }
-              }, function (error, response, body) {
-                console.log(body);
-
-                let resp = JSON.parse(body);
-                if (resp.error) {
-                  counterr++;
-                } else {
-                  count++;
-                }
-                callbackx();
-              });
-
-          }, function done() {
-            console.log("📘 posted to facebook pages news 🚨:" + counterr + " ✅:" + count);
-            callback();
-          });
-
+    gt: id,
+  }, (err, posts) => {
+    db.get(id, (err, doc) => {
+      async.each(_.shuffle(pages), (page, callbackx) => {
+        request.post(`https://graph.facebook.com/${page.id}/feed`, {
+          form: {
+            published: process.env.PORT ? 1 : 0,
+            link: 'https://newsboy.fbook.space/',
+            child_attachments: [{
+              description: posts.docs[0].description,
+              name: posts.docs[0].name,
+              link: `https://newsboy.fbook.space/${id}`,
+              picture: posts.docs[0].url_big,
+            }, {
+              description: posts.docs[1].description,
+              name: posts.docs[1].name,
+              link: `https://newsboy.fbook.space/${posts.docs[1].id}`,
+              picture: posts.docs[1].url_big,
+            }, {
+              description: posts.docs[2].description,
+              name: posts.docs[2].name,
+              link: `https://newsboy.fbook.space/${posts.docs[2].id}`,
+              picture: posts.docs[2].url_big,
+            }, {
+              description: posts.docs[3].description,
+              name: posts.docs[3].name,
+              link: `https://newsboy.fbook.space/${posts.docs[1].id}`,
+              picture: posts.docs[3].url_big,
+            }, {
+              description: posts.docs[4].description,
+              name: posts.docs[4].name,
+              link: `https://newsboy.fbook.space/${posts.docs[4].id}`,
+              picture: posts.docs[4].url_big,
+            }, {
+              description: posts.docs[5].description,
+              name: posts.docs[5].name,
+              link: `https://newsboy.fbook.space/${posts.docs[5].id}`,
+              picture: posts.docs[5].url_big,
+            }, {
+              description: posts.docs[6].description,
+              name: posts.docs[6].name,
+              link: `https://newsboy.fbook.space/${posts.docs[6].id}`,
+              picture: posts.docs[6].url_big,
+            }, {
+              description: posts.docs[7].description,
+              name: posts.docs[7].name,
+              link: `https://newsboy.fbook.space/${posts.docs[7].id}`,
+              picture: posts.docs[7].url_big,
+            }],
+            access_token: page.access_token,
+          },
+        }, (error, response, body) => {
+          const resp = JSON.parse(body);
+          if (resp.error) {
+            counterr++;
+          } else {
+            count++;
+          }
+          callbackx();
+        });
+      }, () => {
+        console.log(`📘 posted to facebook pages news 🚨:${counterr} ✅:${count}`);
+        callback();
       });
-  })
+    });
+  });
 }
 
 function statii(lat, callback) {
-
   db
     .get({
-      id: "newsbg",
-      limit: 1
-    }, function (e, doc) {
-
-      console.log('posting scheduled promo last post statii ' + doc.docs[0].id)
-      promo.post('/newsb/' + doc.docs[0].id, process.env.izvestie_token, doc.docs[0].title, "bgusers", function () {});
+      id: 'newsbg',
+      limit: 1,
+    }, (e, doc) => {
+      console.log(`posting scheduled promo last post statii ${doc.docs[0].id}`);
+      promo.post(`/newsb/${doc.docs[0].id}`, process.env.izvestie_token, doc.docs[0].title, 'bgusers', () => {
+      });
     });
 
-  async.each(pagestoget.rows, function (item, callbackx) {
-    var rtoken = pages[Math.floor(Math.random() * pages.length + 0)].access_token;
-    //var rtoken = process.env.izvestie_token;
-    var url = "https://graph.facebook.com/" + item.id + "/feed?access_token=" + rtoken + "&fields=id,likes,type,full_picture&limit=1";
-    request(url, function (error, response, body) {
 
-      var collect = [];
+  async.each(pagestoget.rows, (itemx, callbackx) => {
+    const rtoken = pages[Math.floor(Math.random() * pages.length + 0)].access_token;
+    const url = `https://graph.facebook.com/${itemx.id}/feed?access_token=${rtoken}&fields=id,likes,type,full_picture&limit=1`;
+    request(url, (error, response, body) => {
+      const collect = [];
       if (!error && response.statusCode == 200) {
-        async
-          .each(JSON.parse(body).data, function (item, callback1) {
+        async.each(JSON.parse(body).data, (item, callback1) => {
+          if (item.likes && item.type === 'link') {
+            request(`https://graph.facebook.com/${item.id}?access_token=${rtoken}&fields=full_picture,message,link,name,created_time`, (error, response, body) => {
+              const insertjson = JSON.parse(body);
+              insertjson.type = 'newsbg';
+              insertjson.title = insertjson.name;
+              insertjson.description = insertjson.message;
+              insertjson.url = insertjson.picture;
+              insertjson.provider = itemx.key;
+              insertjson.source = insertjson.link;
+              insertjson.url_big = insertjson.full_picture;
+              insertjson._id = `${new Date(insertjson.created_time).getTime()}_1`;
 
-            if (item.likes && item.type === "link") {
-              request("https://graph.facebook.com/" + item.id + "?access_token=" + rtoken + "&fields=full_picture,message,link,name,created_time", function (error, response, body) {
-                var insertjson = JSON.parse(body);
-                insertjson.type = 'newsbg';
-                insertjson.title = insertjson.name;
-                insertjson.description = insertjson.message;
-                insertjson.url = insertjson.picture;
-                insertjson.source = insertjson.link;
-                insertjson.url_big = insertjson.full_picture;
-                insertjson._id = new Date(insertjson.created_time).getTime() + '_1';
-                db.get(insertjson._id, function (err, data) {
-                  if (err) {
-                    db
-                      .put(insertjson, function (err, zer) {
-                        post(insertjson._id, function (zzmata) {
-                          callback1();
-                        });
 
+              db.get(insertjson._id, (err, data) => {
+                if (err) {
+                  db
+                    .put(insertjson, (err, zer) => {
+                      post(insertjson._id, (zzmata) => {
+                        callback1();
                       });
-                  } else {
-                    callback1();
-                  }
-                });
-              })
-            } else {
-              callback1();
-            }
-          }, function done() {
-            callbackx();
-          });
+                    });
+                } else {
+                  callback1();
+                }
+              });
+            });
+          } else {
+            callback1();
+          }
+        }, () => {
+          callbackx();
+        });
       } else {
         callbackx();
       }
     });
-  }, function done() {
+  }, () => {
     callback();
   });
 }
 
 if (!process.env.PORT) {
-  statii('1', function (data) {
+  statii('1', (data) => {
     console.log(data);
-
-  })
-
+  });
 }
 
 module.exports = {
-  statii: statii
+  statii,
 };
 //
