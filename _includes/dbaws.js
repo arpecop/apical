798845e8cc7f6b329b51c635ab90ea40cd2ef69c
Rudi <rudix@mac.local>
2017-08-19 12:14:45 +0300

    c

--- _includes/dbaws.js
+++ _includes/dbaws.js
@@ -6,76 +6,82 @@ const fs = require('fs');
 const PouchDB = require('pouchdb');
 
 const db = new PouchDB('http://1:1@pouchdb.herokuapp.com/db');
+
+const db2 = new PouchDB(
+  process.env.PORT
+    ? 'http://pouchdb.herokuapp.com/api'
+    : 'http://mac.local:3001/api',
+);
 const db1 = new PouchDB('http://1:1@pouchdb.herokuapp.com/content');
 
 function put(jsonx, callback) {
-  db
-    .get(jsonx._id, (err, old_doc) => {
-      const json = {
-        _id: jsonx._id
-          ? jsonx._id
-          : `${new Date().getTime()}_${Math.floor((Math.random() * 10) + 1)}`,
-        type: jsonx.type
-          ? (jsonx.type || jsonx._id)
-          : undefined,
-        time: new Date().getTime(),
-        _rev: err
-          ? undefined
-          : old_doc._rev,
-        value: jsonx,
-      };
-      db.put(json, (err, cap) => {
-        callback(null, cap);
-      });
+  db.get(jsonx._id, (err, old_doc) => {
+    const json = {
+      _id: jsonx._id
+        ? jsonx._id
+        : `${new Date().getTime()}_${Math.floor(Math.random() * 10 + 1)}`,
+      type: jsonx.type ? jsonx.type || jsonx._id : undefined,
+      time: new Date().getTime(),
+      _rev: err ? undefined : old_doc._rev,
+      value: jsonx,
+    };
+    db.put(json, (err, cap) => {
+      callback(null, cap);
     });
+  });
 }
 
 function get(id, callback) {
   if (id) {
-    const sid = (typeof id === 'object')
-      ? id.id || id._id
-      : id;
+    const sid = typeof id === 'object' ? id.id || id._id : id;
     if (id.limit) {
-      db
-        .query(`i/${id.id}`, Object.assign(id, {
+      db.query(
+        `i/${id.id}`,
+        Object.assign(id, {
           descending: true,
-          skip: id.skip
-            ? id.skip
-            : 0,
-          start_key: id.gt
-            ? id.gt
-            : undefined,
-        }), (err, body) => {
+          skip: id.skip ? id.skip : 0,
+          start_key: id.gt ? id.gt : undefined,
+        }),
+        (err, body) => {
           if (!err) {
             const arr = [];
-            Promise.all(body.rows.map(item => new Promise(((cb, rj) => {
-              arr.push(Object.assign(item.value.value, {
-                key: item.id,
-                id: item.id,
-                _date: new Date(item.value.time),
-              }));
-              cb();
-            }))))
-              .then((data) => {
-                callback(null, {
-                  docs: arr,
-                });
+            Promise.all(
+              body.rows.map(
+                item =>
+                  new Promise((cb, rj) => {
+                    arr.push(
+                      Object.assign(item.value.value, {
+                        key: item.id,
+                        id: item.id,
+                        _date: new Date(item.value.time),
+                      }),
+                    );
+                    cb();
+                  }),
+              ),
+            ).then((data) => {
+              callback(null, {
+                docs: arr,
               });
+            });
           } else {
             callback(err);
           }
-        });
+        },
+      );
     } else {
-      db
-        .get(sid, (err, doc) => {
-          if (err) {
-            callback({}, {});
-          } else {
-            callback(null, Object.assign(doc.value, {
+      db.get(sid, (err, doc) => {
+        if (err) {
+          callback({}, {});
+        } else {
+          callback(
+            null,
+            Object.assign(doc.value, {
               _id: doc._id,
-            }));
-          }
-        });
+            }),
+          );
+        }
+      });
     }
   } else {
     callback({}, {});
@@ -90,5 +96,6 @@ module.exports = {
   insert: put,
   put,
   db1,
+  db2,
   query: db.query,
 };
--- slack.js
+++ slack.js
@@ -1,45 +1,14 @@
-var slack = require('slack')
-//
-let bot = slack
-    .rtm
-    .client()
-let token = 'xoxp-220341632595-219880865249-221094636467-ba63c499bcd6e0de5a3c2a9d20554f9a'
+const db = require('./_includes/dbaws.js');
 
-//bot.listen({token})
-
-slack
-    .channels
-    .create({
-        'token': token,
-        'name': 'test'
-    }, function (err, data) {
-        console.log(err);
-
-    })
-
-for (i = 0; i < 100; i++) {
-    slack
-        .chat
-        .postMessage({
-            'token': token,
-            'channel': 'test',
-            'text': {
-                '_id': new Date().getTime()
-            }
-        }, function (err, data) {
-            console.log(err);
-
-        })
-
-}
-
-slack
-    .search
-    .messages({
-        'token': token,
-
-        'query': '"emp"'
-    }, function (err, data) {
-        console.log(data);
-
-    })
+db.db2.get('1502872229000_1', (err, data) => {
+  // console.log(data);
+});
+db.db2.get(
+  {
+    id: 'newsbg',
+    limit: 20,
+  },
+  (err, posts) => {
+    console.log(posts);
+  },
+);
