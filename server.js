b2270ac13376bfaea6fd206d5de88ad1dc83b9f3
Rudi <rudix.lab@gmail.com>
2017-09-04 21:53:53 +0300

    c

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ if (cluster.isMaster) {
   request.get('http://apicall2.herokuapp.com/', () => {});
   request.get('http://chimpsnap.herokuapp.com/', () => {});
 
-  async.waterfall(
+  async.each(
     [
       // (cb) => {
       (cb) => {
