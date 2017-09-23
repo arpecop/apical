bbf62e4866411392f50274dc482ef67f38e75c36
Rudi <rudix.lab@gmail.com>
2017-09-23 18:50:43 +0300

    c

--- _statii.js
+++ _statii.js
@@ -242,6 +242,12 @@ async function statii(params, callback) {
   );
   await scheduled_post(
     'newsbg', // view to retrieve latest post and send the title
+    'app/newsboy', // before the _id
+    process.env.izvestie_token,
+    'bgusers', // userbase on localhost to randomize
+  );
+  await scheduled_post(
+    'newsbg', // view to retrieve latest post and send the title
     'app/newsfly', // before the _id
     process.env.izvestie_token,
     'bgusers', // userbase on localhost to randomize
