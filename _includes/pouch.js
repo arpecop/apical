4c968105a1037b32d5740a34a8482808f500279f
Rudi <rudix.lab@gmail.com>
2017-10-16 18:31:53 +0300

    c

--- _includes/pouch.js
+++ _includes/pouch.js
@@ -5,17 +5,17 @@ const allusers = new PouchDB(`${__dirname}/db/allusers`);
 
 const wordb = new PouchDB('/tmp/allusers');
 const _ = require('underscore');
+const async = require('async');
 
 //
-let levelup = require('levelup');
-let leveldown = require('leveldown');
+const levelup = require('levelup');
+const leveldown = require('leveldown');
 
 // 1) Create our store
-let db = levelup(leveldown('./mydb'));
 
 //
 
-function gimmethousend(db, callback) {
+function gimmethousend2(db, callback) {
   allusers.get(`0_${db}`, (err, count) => {
     const rd = Math.floor(Math.random() * count.total) + 0;
     allusers.get(`${db}_${rd}`, (err, chunk) => {
@@ -26,31 +26,53 @@ function gimmethousend(db, callback) {
 
 // /////
 
-async function gimmethousend1(db, callback) {
-  const dbxx = new PouchDB(`/tmp/${db}x`);
+async function gimmethousend(db, callback) {
+  const dbx = levelup(leveldown(`/tmp/${db}xx`));
+  dbx.get('next', (err, next) => {
+    if (err) {
+      allusers.get(`0_${db}`, (err, count) => {
+        const whatever = _.flatten(
+          new Array(150).fill(
+            _.shuffle(_.keys(new Array(count.total).join('0').split(''))),
+          ),
+        ).map((val, _id) => ({
+          _id,
+          val,
+        }));
 
-  allusers.get(`0_${db}`, (err, count) => {
-    // _.shuffle(_.keys(emptyArray)
-    const whatever = _.flatten(
-      new Array(150).fill(
-        _.shuffle(_.keys(new Array(count.total).join('0').split(''))),
-      ),
-    ).map((val, index) => val);
-    // { _id: `${index}`, val }
-    console.log(JSON.stringify(whatever));
-    // whatever.push({ _id: 'counter', val: '0' });
-
-    const rd = Math.floor(Math.random() * count.total) + 0;
-    console.log(rd);
-
-    allusers.get(`${db}_${rd}`, (err, chunk) => {
-      callback(chunk.users);
-    });
+        async.each(
+          whatever,
+          (val, cb) => {
+            dbx.put(val._id, val.val, (err) => {
+              // console.log(val);
+              cb();
+            });
+          },
+          (err) => {
+            dbx.put('next', 0, () => {
+              allusers.get(`${db}_${whatever[0].val}`, (err, chunk) => {
+                callback(chunk.users);
+              });
+            });
+          },
+        );
+        // whatever.push({ _id: 'counter', val: '0' });
+      });
+    } else {
+      dbx.put('next', Math.round(next.toString()) + 1, () => {
+        dbx.get(next.toString(), (err, val) => {
+          allusers.get(`${db}_${val.toString()}`, (err, chunk) => {
+            callback(chunk.users);
+          });
+        });
+      });
+    }
   });
 }
 
 if (!process.env.PORT) {
-  gimmethousend1('cookie', (data) => {});
+  // gimmethousend('bgusers', (data) => {
+  // });
 }
 
 module.exports = {
