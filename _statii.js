3b54cd25c911ad98ae5525241bceb79fa966d1d1
rudi <rudix.lab@gmail.com>
2018-01-08 17:55:35 +0200

    c

--- _statii.js
+++ _statii.js
@@ -17,7 +17,7 @@ const s3 = new AWS.S3({
 
 const localdb = levelup(leveldown('/tmp/localx1'));
 
-const db = new PouchDB('http://1:1@pouchdb.herokuapp.com/api');
+const db = new PouchDB('http://1:1@pouchdb.herokuapp.com/db');
 // const db = require('nano')('http://pouchdb.herokuapp.com/api/');
 
 // const pagestoget = require (`${__dirname}/_includes/source.json`);
