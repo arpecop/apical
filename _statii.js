070bf1c3c9e1a2bfd48b2b8f1416c447e0b3c0f8
Your Name <you@example.com>
2018-04-26 10:01:59 +0300

    c

--- _statii.js
+++ _statii.js
@@ -45,8 +45,7 @@ async function postPages() {
     'https://arpecop.gitlab.io/izteglisi/post/',
     'https://wt-rudix_lab-gmail_com-0.sandbox.auth0-extend.com/share/',
     'https://now-yjirixakkv.now.sh/',
-    'http://izteglisi.club/',
-    'http://sharticle.herokuapp.com/'
+    'http://izteglisi.club/' 
   ]
   let domain = _.shuffle(domains)[0]
   return new Promise(resolve => {
