80f087cd0f7d5fc32117e4fbc494cb65c07c1340
Rudi <rudix.lab@gmail.com>
2018-10-20 21:09:32 +0300

    c

--- _twitter.js
+++ _twitter.js
@@ -1,17 +1,16 @@
-
 const PouchDB = require('pouchdb');
 const request = require('request');
 const { exec } = require('child_process');
 const async = require('async');
 const levelup = require('levelup');
 const leveldown = require('leveldown');
-const md5 = require('md5');
-const jsonizehtml = require('html2json').html2json;
-const sanitizeHtml = require('sanitize-html');
+
 
 const statii = require('./_statii.js');
 
-const localdb = levelup(leveldown(process.env.PORT ? '/tmp/twitter' : `/tmp/${new Date()}`));
+const localdb = levelup(
+  leveldown(process.env.PORT ? '/tmp/twitter' : `/tmp/${new Date()}`),
+);
 const db = new PouchDB('http://1:1@pouchdb.herokuapp.com/db');
 
 function postDynamo(json, callback) {
@@ -25,7 +24,6 @@ function postDynamo(json, callback) {
     callback();
   });
 }
-
 
 function populatedb(id, callback) {
   if (id) {
@@ -43,7 +41,6 @@ function populatedb(id, callback) {
   }
 }
 
-
 async function getFreshOnes(posts, type) {
   const arr = [];
   return new Promise((resolve) => {
@@ -51,38 +48,42 @@ async function getFreshOnes(posts, type) {
       posts,
       (post, cb) => {
         if (post) {
-          populatedb(process.env.PORT ? post.id : new Date().getTime().toString(), (exist) => {
-            if (exist) {
-              db.put(
-                {
-                  _id: post.id,
-                },
-                () => {
-                  console.log(post);
+          populatedb(
+            process.env.PORT ? post.id : new Date().getTime().toString(),
+            (exist) => {
+              if (exist) {
+                db.put(
+                  {
+                    _id: post.id,
+                  },
+                  () => {
+                    console.log(post);
 
-                  const objectDefined = {
-                    ...post,
-                    sortable: [type],
-                    time: Math.round(post.id),
-                    id: undefined,
-                    title: post.text,
-                    image: post.images ? post.images[0] : undefined,
-                  };
-
-                  postDynamo(objectDefined, (params) => {
+                    const objectDefined = {
+                      ...post,
+                      sortable: [type],
+                      time: Math.round(post.id),
+                      id: undefined,
+                      title: post.text,
+                      image: post.images ? post.images[0] : undefined,
+                    };
                     db.put(objectDefined, (err) => {
-                    // console.log(err);
-
-                      cb();
-                      arr.push(objectDefined);
+                      if (objectDefined.image) {
+                        postDynamo(objectDefined, (params) => {
+                          cb();
+                          arr.push(objectDefined);
+                        });
+                      } else {
+                        cb();
+                      }
                     });
-                  });
-                },
-              );
-            } else {
-              cb();
-            }
-          });
+                  },
+                );
+              } else {
+                cb();
+              }
+            },
+          );
         } else {
           cb();
         }
@@ -98,17 +99,16 @@ async function getTl(user) {
   console.log(user);
   return new Promise((resolve) => {
     if (user.length > 2) {
-      exec(`./node_modules/scrape-twitter/bin/scrape-twitter.js search --query="${user}" --count 5  --type latest`, (err, stdout, stderr) => {
-        if (err) {
-          // node couldn't execute the command
-
-          resolve();
-        } else {
-          resolve(JSON.parse(stdout));
-        }
-
-      // the *entire* stdout and stderr (buffered)
-      });
+      exec(
+        `./node_modules/scrape-twitter/bin/scrape-twitter.js search --query="${user}" --count 5  --type latest`,
+        (err, stdout, stderr) => {
+          if (err) {
+            resolve();
+          } else {
+            resolve(JSON.parse(stdout));
+          }
+        },
+      );
     } else {
       resolve();
     }
@@ -118,7 +118,9 @@ const timelinesArr = require(`${__dirname}/_includes/sources/twitter.js`);
 async function gowork(params, callback) {
   const allBg = [].concat.apply(
     [],
-    await Promise.all(timelinesArr.bgQueries.map(async q => await getTl(q, 'twitterbg'))),
+    await Promise.all(
+      timelinesArr.bgQueries.map(async q => await getTl(q, 'twitterbg')),
+    ),
   );
   // await getFreshOnes(allEn, 'twitteren');
   const freshBg = await getFreshOnes(allBg, 'twitterbg');
@@ -131,7 +133,7 @@ async function gowork(params, callback) {
 if (!process.env.PORT) {
   // test();
 
-  gowork(1, () => { });
+  gowork(1, () => {});
   process.stdin.resume();
 }
 
