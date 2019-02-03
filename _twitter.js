32528e526cab86971c584eb2397a84efe85e5999
rudix <rudix@mac.local>
2019-02-03 14:03:41 +0200

    c

--- _twitter.js
+++ _twitter.js
@@ -71,6 +71,7 @@ async function getFreshOnes(posts, type) {
                     _id: Math.round(post.id).toString(),
                     id: undefined,
                     title: post.text,
+                    date: new Date().getTime().toString(),
                     image: post.images ? post.images[0] : undefined,
                   };
 
