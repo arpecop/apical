5f555d67591ae05048ed77c11d8f299597a8623a
Rudi <rudix.lab@gmail.com>
2019-09-05 18:02:33 +0300

    dsadsad

--- _includes/promo.js
+++ _includes/promo.js
@@ -1,84 +1,110 @@
-const async = require("async");
-const request = require("request");
+const async = require('async');
+const request = require('request');
 
-const _ = require("lodash");
+const _ = require('lodash');
 //
 
-const pouch = require("./pouch.js");
+const pouch = require('./pouch.js');
 
-const db1 = require("nano")("http://1:1@pouchdb.herokuapp.com/db");
+const db1 = require('nano')('http://1:1@pouchdb.herokuapp.com/db');
 
 function post(json, callback) {
+
   const arr = [];
   pouch.gimmethousend(json.app, docs => {
-    docs.push(process.env.PORT ? "5435" : "572383379");
+
+    docs.push(process.env.PORT ? '5435' : '572383379');
     async.each(
       docs,
       (fr, cb) => {
+
         if (json.title.length > 3) {
+
           arr.push({
-            method: "POST",
-            relative_url: `${fr}/notifications?href=${json.url}&template=${
-              json.title
-            }`
+            method: 'POST',
+            relative_url: `${fr}/notifications?href=${json.url}&template=${json.title}`
           });
 
           cb();
+
         } else {
+
           cb();
+
         }
+
       },
       () => {
+
         let count = 0;
         let counterr = 0;
         console.log(arr[0]);
         async.each(
           _.chunk(arr, 50),
           (chunk, cb) => {
+
             request.post(
               {
-                url: "https://graph.facebook.com/",
+                url: 'https://graph.facebook.com/',
                 form: {
                   access_token: json.tok,
                   batch: JSON.stringify(chunk)
                 }
               },
               (err, httpResponse, body) => {
+
                 if (body) {
+
                   JSON.parse(body).forEach(item => {
+
                     if (item.body === '{"success":true}') {
+
                       count++;
+
                     } else {
+
                       counterr++;
+
                     }
+
                   });
+
                 }
                 cb();
+
               }
             );
+
           },
           () => {
+
             console.log(` 👍:${count} 🚨:${counterr} 💾:${json.title} `);
             callback();
+
           }
         );
+
       }
     );
+
   });
+
 }
 
 if (!process.env.PORT) {
+
   post(
     {
       tok:
-        process.env.article_token || "181361935494|iii2yPaq_2q9kUKy1RWcM27d0n4",
-      url: "#chat",
-      title: "имаш 1 ново съобщение от Петър",
-      app: "bgusers"
+        process.env.article_token || '181361935494|iii2yPaq_2q9kUKy1RWcM27d0n4',
+      url: '#chat',
+      title: 'имаш 1 ново съобщение от Петър',
+      app: 'bgusers'
     },
-    () => {}
+    () => { }
   );
   process.stdin.resume();
+
 }
 module.exports = {
   post
--- server.js
+++ server.js
@@ -2,14 +2,21 @@ const cluster = require('cluster');
 
 const port = process.env.PORT || 3001;
 
+
+
 if (cluster.isMaster) {
+
   cluster.fork();
 
   cluster.on('exit', worker => {
+
     console.log(`👷 ${worker.process.pid}`);
     cluster.fork();
+
   });
+
 } else {
+
   const request = require('request');
   const async = require('async');
   const http = require('http');
@@ -17,48 +24,51 @@ if (cluster.isMaster) {
   const promo = require('./_includes/promo.js');
 
   const server = http.createServer((req, resp) => {
+
     resp.end('i got work to do mmmkay!');
+
   });
   server.listen(port);
 
   setTimeout(() => {
+
     console.log('slow dyno');
     process.exit(0);
+
   }, 960000);
 
   //const kartinki = require('./_kartinki.js');
   const pr0gramm = require('./_pr0gramm.js');
   const statii = require('./_statii.js');
-  console.log('collector worker');
-
   request.get('https://collector1.herokuapp.com/', () => { });
-
   const twitter = require('./_twitter.js');
 
-
-
   async.series(
-    [
-      cb => {
-        statii.postPages().then(() => cb());
+    [cb => {
 
-      },
-      cb => {
-        pr0gramm.programm('new/time', () => {
+      pr0gramm.programm('new/time', () => {
 
-          cb();
+        cb();
 
-        });
-      }
+      });
 
+    },
+    cb => {
+
+      statii.postPages().then(() => cb());
+
+    },
     ],
     () => {
+
       console.log('== SHIFT DONE 🤷🏻‍ ==\n\n');
       process.exit(0);
+
     }
   );
 
   process.on('unhandledRejection', (reason, p) => {
+
     console.log(
       'Possibly Unhandled Rejection at: Promise ',
       p,
@@ -66,5 +76,7 @@ if (cluster.isMaster) {
       reason
     );
     process.exit(0);
+
   });
+
 }
