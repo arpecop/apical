9f7bdc6aacc23f50e8ede88de5c8fa7bdb54e746
Rudi <rudix.lab@gmail.com>
2019-10-21 09:59:56 +0300

    c

--- _twitter.js
+++ _twitter.js
@@ -7,10 +7,10 @@ const levelup = require('levelup');
 const leveldown = require('leveldown');
 
 const localdb = levelup(leveldown(process.env.PORT ? '/tmp/twitter' : `/tmp/${new Date()}`));
-const urlx = 'https://arpecop.serveo.net/twitter';
+const urlx = 'http://3.120.176.89/twitter';
 
 const db = require('nano')('http://1:1@pouchdb.herokuapp.com/db');
-const db1 = require('nano')('https://arpecop.serveo.net/twitter');
+const db1 = require('nano')('http://3.120.176.89/twitter');
 request.get(`${urlx}/_design/api/_view/feed?reduce=false&skip=0&limit=1`, () => {});
 request.get(`${urlx}/_design/api/_view/users?reduce=false&skip=0&limit=1`, () => {});
 request.get(`${urlx}/_design/api/_view/tags?reduce=false&skip=0&limit=1`, () => {});
