5281234b8e80bc7eb1dbccc74bd62973c0b4452b
Radoslav Georgiev <rudix.lab@gmail.com>
2020-02-01 15:01:07 +0200

    c

--- _twitter.js
+++ _twitter.js
@@ -1,155 +1,183 @@
-const request = require('request');
+const request = require('request')
 
-const { exec } = require('child_process');
-const async = require('async');
-const levelup = require('levelup');
+const { exec } = require('child_process')
+const async = require('async')
+const levelup = require('levelup')
 
-const leveldown = require('leveldown');
+const leveldown = require('leveldown')
 
-const localdb = levelup(leveldown(process.env.PORT ? '/tmp/twitter' : `/tmp/${new Date()}`));
-const urlx = 'http://3.120.176.89/twitter';
+const localdb = levelup(
+    leveldown(process.env.PORT ? '/tmp/twitter' : `/tmp/${new Date()}`)
+)
+const urlx = 'http://3.120.176.89/twitter'
 
-const db = require('nano')('http://1:1@pouchdb.herokuapp.com/db');
-const db1 = require('nano')('http://pouchdb.herokuapp.com/twitter');
-request.get(`${urlx}/_design/api/_view/feed?reduce=false&skip=0&limit=1`, () => {});
-request.get(`${urlx}/_design/api/_view/users?reduce=false&skip=0&limit=1`, () => {});
-request.get(`${urlx}/_design/api/_view/tags?reduce=false&skip=0&limit=1`, () => {});
+const db = require('nano')('http://1:1@db.arpecop.xyz/db')
+const db1 = require('nano')('http://db.arpecop.xyz/twitter')
+request.get(
+    `${urlx}/_design/api/_view/feed?reduce=false&skip=0&limit=1`,
+    () => {}
+)
+request.get(
+    `${urlx}/_design/api/_view/users?reduce=false&skip=0&limit=1`,
+    () => {}
+)
+request.get(
+    `${urlx}/_design/api/_view/tags?reduce=false&skip=0&limit=1`,
+    () => {}
+)
 function populatedb(id, callback) {
-  if (id) {
-    localdb.get(id, err => {
-      if (err) {
-        localdb.put(id, 'c', () => {
-          callback(true);
-        });
-      } else {
-        callback(false);
-      }
-    });
-  } else {
-    callback(false);
-  }
+    if (id) {
+        localdb.get(id, err => {
+            if (err) {
+                localdb.put(id, 'c', () => {
+                    callback(true)
+                })
+            } else {
+                callback(false)
+            }
+        })
+    } else {
+        callback(false)
+    }
 }
 
 async function getFreshOnes(posts, type) {
-  const arr = [];
-  return new Promise(resolve => {
-    async.each(
-      posts,
-      (post, cb) => {
-        if (post) {
-          populatedb(process.env.PORT ? post.id : new Date().getTime().toString(), exist => {
-            if (exist) {
-              db.insert(
-                {
-                  _id: post.id,
-                },
-                () => {
-                  const objectDefined = {
-                    ...post,
-                    sortable: [type],
-                    time: Math.round(post.id),
-                    _id: Math.round(post.id).toString(),
-                    title: post.text,
-                    text: null,
-                    date: new Date().getTime().toString(),
-                    image: post.images ? post.images[0] : undefined,
-                  };
+    const arr = []
+    return new Promise(resolve => {
+        async.each(
+            posts,
+            (post, cb) => {
+                if (post) {
+                    populatedb(
+                        process.env.PORT
+                            ? post.id
+                            : new Date().getTime().toString(),
+                        exist => {
+                            if (exist) {
+                                db.insert(
+                                    {
+                                        _id: post.id,
+                                    },
+                                    () => {
+                                        const objectDefined = {
+                                            ...post,
+                                            sortable: [type],
+                                            time: Math.round(post.id),
+                                            _id: Math.round(post.id).toString(),
+                                            title: post.text,
+                                            text: null,
+                                            date: new Date()
+                                                .getTime()
+                                                .toString(),
+                                            image: post.images
+                                                ? post.images[0]
+                                                : undefined,
+                                        }
 
-                  db1.insert(objectDefined, (e, doc) => {
-                    console.log('====================================');
-                    console.log(e ? e.statusCode : doc);
-                    console.log('====================================');
-                    cb();
-                  });
-                },
-              );
-            } else {
-              cb();
+                                        db1.insert(objectDefined, (e, doc) => {
+                                            console.log(
+                                                '===================================='
+                                            )
+                                            console.log(e ? e.statusCode : doc)
+                                            console.log(
+                                                '===================================='
+                                            )
+                                            cb()
+                                        })
+                                    }
+                                )
+                            } else {
+                                cb()
+                            }
+                        }
+                    )
+                } else {
+                    cb()
+                }
+            },
+            () => {
+                resolve(arr)
             }
-          });
-        } else {
-          cb();
-        }
-      },
-      () => {
-        resolve(arr);
-      },
-    );
-  });
+        )
+    })
 }
 
 async function getTl(q, type) {
-  return new Promise(resolve => {
-    const q1 =
-      type === 'user'
-        ? `./node_modules/scrape-twitter/bin/scrape-twitter.js timeline ${q} --count 20`
-        : `./node_modules/scrape-twitter/bin/scrape-twitter.js search --query="${q}" --count 20  --type latest`;
-    if (q.length > 2) {
-      exec(q1, (err, stdout) => {
-        if (err || !stdout) {
-          console.log(q);
+    return new Promise(resolve => {
+        const q1 =
+            type === 'user'
+                ? `./node_modules/scrape-twitter/bin/scrape-twitter.js timeline ${q} --count 20`
+                : `./node_modules/scrape-twitter/bin/scrape-twitter.js search --query="${q}" --count 20  --type latest`
+        if (q.length > 2) {
+            exec(q1, (err, stdout) => {
+                if (err || !stdout) {
+                    console.log(q)
 
-          resolve();
+                    resolve()
+                } else {
+                    resolve(JSON.parse(stdout))
+                }
+            })
         } else {
-          resolve(JSON.parse(stdout));
+            resolve()
         }
-      });
-    } else {
-      resolve();
-    }
-  });
+    })
 }
-const { bgQueries, enQueries, bgUsers } = require(`./_includes/sources/twitter.js`);
+const {
+    bgQueries,
+    enQueries,
+    bgUsers,
+} = require(`./_includes/sources/twitter.js`)
 async function queries(quries, type) {
-  return new Promise(resolve => {
-    async.eachLimit(
-      quries,
-      8,
-      (q, callback) => {
-        getTl(q, 'query').then(data => {
-          getFreshOnes(data, type).then(() => callback());
-        });
-      },
-      () => {
-        resolve({});
-      },
-    );
-  });
+    return new Promise(resolve => {
+        async.eachLimit(
+            quries,
+            8,
+            (q, callback) => {
+                getTl(q, 'query').then(data => {
+                    getFreshOnes(data, type).then(() => callback())
+                })
+            },
+            () => {
+                resolve({})
+            }
+        )
+    })
 }
 
 async function users(queries, type) {
-  return new Promise(resolve => {
-    async.eachLimit(
-      queries,
-      8,
-      (q, callback) => {
-        getTl(q, 'user').then(data => {
-          getFreshOnes(data, type).then(() => callback());
-        });
-      },
-      () => {
-        resolve({});
-      },
-    );
-  });
+    return new Promise(resolve => {
+        async.eachLimit(
+            queries,
+            8,
+            (q, callback) => {
+                getTl(q, 'user').then(data => {
+                    getFreshOnes(data, type).then(() => callback())
+                })
+            },
+            () => {
+                resolve({})
+            }
+        )
+    })
 }
 // dsadas
 
 async function gowork(params, callback) {
-  await users(bgUsers, 'bgNews');
-  await queries(bgQueries, 'twitterbg');
-  await queries(enQueries, 'twitteren');
+    await users(bgUsers, 'bgNews')
+    await queries(bgQueries, 'twitterbg')
+    await queries(enQueries, 'twitteren')
 
-  console.log('== D O N E   T W I T T E R ==');
-  callback({});
+    console.log('== D O N E   T W I T T E R ==')
+    callback({})
 }
 
 if (!process.env.PORT) {
-  gowork(1, () => {});
+    gowork(1, () => {})
 
-  process.stdin.resume();
+    process.stdin.resume()
 }
 // dasddsad
 module.exports = {
-  gowork,
-};
+    gowork,
+}
