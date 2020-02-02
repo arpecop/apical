05fb1f19428db874318cc6cc284c815fda41e14c
Radoslav Georgiev <rudix.lab@gmail.com>
2020-02-02 08:03:28 +0200

    c

--- _twitter.js
+++ _twitter.js
@@ -12,7 +12,7 @@ const localdb = levelup(
 const urlx = 'http://3.120.176.89/twitter'
 
 const db = require('nano')('http://1:1@pouchdb.herokuapp.com/db')
-const db1 = require('nano')('https://pouchdb.herokuapp.com/twitter')
+const db1 = require('nano')('https://db.arpecop.xyz/twitter')
 request.get(
     `${urlx}/_design/api/_view/feed?reduce=false&skip=0&limit=1`,
     () => {}
