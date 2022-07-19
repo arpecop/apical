f8a07ea94da371a67ffa5590e8fd07f5fd5061ad
arpecop <rudix.lab@gmail.com>
2022-07-19 17:50:30 +0300

    feat(pill): phase 2 run

--- dirbg/perpage.mjs
+++ dirbg/perpage.mjs
@@ -18,7 +18,7 @@ function run_bitch () {
     res.json().then(tasks => {
       async.eachLimit(
         tasks.rows,
-        250,
+        30,
         function (task, callback) {
           gethtml(task.value.url).then(html_full => {
             console.log(task.value.url + ' ' + html_full.length)
